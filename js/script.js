86fe41193695c317cdb219881eb14432f1018105
Dan <daniel.munozdev@gmail.com>
2017-09-27 14:26:25 -0700

    Add query selector for images, modal view and looped through images

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const images = document.querySelectorAll('img');
+const modalView = document.querySelector('.modal-view');
+console.log(images, modalView);
+
+for (let i = 0; i < images.length; i++) {
+  console.log(images[i]);
+  const image = images[i];
+}
