7782d4451fec4b621ac31ac4a59fe0d54c0f2c57
Dan <daniel.munozdev@gmail.com>
2017-09-27 14:30:08 -0700

    Add event listeners to images and modal-view div

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
 const images = document.querySelectorAll('img');
 const modalView = document.querySelector('.modal-view');
 console.log(images, modalView);
+modalView.addEventListener('click', onModalClick);
+
+function onImageClick(event) {
+  console.log('Hello clicked img');
+  const image = createImage(event.currentTarget.src, event.currentTarget.alt);
+
+  document.body.classList.add('no-scroll');
+  modalView.style.top = window.pageYOffset + 'px';
+  modalView.classList.remove('hidden');
+
+  modalView.appendChild(image);
+}
+
+function onModalClick() {
+  hideModal();
+}
+
+function hideModal() {
+  document.body.classList.remove('no-scroll');
+  modalView.classList.add('hidden');
+  modalView.innerHTML = '';
+}
 
 for (let i = 0; i < images.length; i++) {
   console.log(images[i]);
   const image = images[i];
+  image.addEventListener('click', onImageClick);
+}
+
+function createImage(src, alt) {
+  const image = document.createElement('img');
+  image.src = src;
+  image.alt = alt;
+  return image;
 }
